9326f1644fdf686101a6d18414f0b8933f57690a
iremsevvalkaya <iremsevvalceng@gmail.com>
2023-08-01 20:41:28 +0300

    uygulamaDersi3.kt

---
+++ app/src/main/java/com/example/uygulamadersi/uygulamaDersi3.kt
@@ -0,0 +1,21 @@
+package com.example.uygulamadersi
+
+/*Verilen bir listedeki tüm elemanları toplayan bir fonksiyon yazın.
+fun main(){
+    val numbers = listOf(1,2,3,4,5,6,7,8,8,9,10)
+    println(numbers.sum())
+    var sum = 0
+    numbers.forEach{
+      sum += it
+
+    }
+    println(sum)
+} */
+/*Bir listedeki tek sayıları filtreleyen bir fonksiyon yazın
+fun tekSayilar(l: List<Int>) = l.filter { it % 2 != 0 }
+
+fun main() {
+    val ls = listOf(1, 2, 3, 4, 5, 6, 7, 8, 9)
+    println("Tek sayilar: ${tekSayilar(ls)}")
+} */
+
