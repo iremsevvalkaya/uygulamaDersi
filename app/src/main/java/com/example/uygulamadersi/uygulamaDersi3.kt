16a3205d5d92b107ae46e0a4d607e334be06ba10
iremsevvalkaya <iremsevvalceng@gmail.com>
2023-08-01 20:51:23 +0300

    uygulamaDersi3.kt

--- app/src/main/java/com/example/uygulamadersi/uygulamaDersi3.kt
+++ app/src/main/java/com/example/uygulamadersi/uygulamaDersi3.kt
@@ -18,4 +18,14 @@ fun main() {
     val ls = listOf(1, 2, 3, 4, 5, 6, 7, 8, 9)
     println("Tek sayilar: ${tekSayilar(ls)}")
 } */
+/*İki listenin elemanlarını birleştirip sıralayan bir fonksiyon yazın
+package com.example.challenge.week3.Question3
 
+fun birlestir(list1:ArrayList<Int>,list2:ArrayList<Int>): List<Int>{
+     return (list1 + list2).sorted()
+}
+
+fun main() {
+print(birlestir(arrayListOf(1,2,3), arrayListOf(1,2,3)))
+}
+ */
